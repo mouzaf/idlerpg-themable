mouzaf/idlerpg-themable

commit a76002565e9eb25b3f7b682e5b2a02aa2324289c

bot.pl: modify_item names unique items
Previously it would just be the type. Now we can name uniques.

Signed-off-by: Phil Carmody <fatphil@asdf.org>

bot.pl
@@ -1839,12 +1839,12 @@ sub modify_item($) {
         }
         $change = rewrite_event($change, $player, undef); # random number not used currently
         my $type = $items[$typeid];
+        my $suffix="";
+        if ($rps{$player}{item}[$typeid] =~ /(\D)$/) { $suffix=$1; $type=$uniques[$typeid]->{desc}; }
         $change = "${player}$change" .
             " $player\'s $type $change[$good] 10% of its effectiveness.";
         chanmsg_l($change);
 
-        my $suffix="";
-        if ($rps{$player}{item}[$typeid] =~ /(\D)$/) { $suffix=$1; }
         $rps{$player}{item}[$typeid] = int(int($rps{$player}{item}[$typeid]) * (1+$_[0]*.1));
         $rps{$player}{item}[$typeid].=$suffix;
     }

commit b2c8b38056f8d86c6533b9bf33cd0079b628dc5b

bot.pl: fix horribly verbose unique item description in modify
Would use the full description, which is just noise in that context.

Issue was introduced in commit a76002565e9eb25b3f7b682e5b2a02aa2324289c
Author: Phil Carmody <fatphil@asdf.org>
Date:   Thu Feb 9 21:11:37 2012 +0200

    bot.pl: modify_item names unique items

Signed-off-by: Phil Carmody <pc+dev@asdf.org>

bot.pl
@@ -1890,7 +1890,7 @@ sub modify_item($) {
         $change = rewrite_event($change, $player, undef); # random number not used currently
         my $type = $items[$typeid];
         my $suffix="";
-        if ($rps{$player}{item}[$typeid] =~ /(\D)$/) { $suffix=$1; $type=$uniques[$typeid]->{desc}; }
+        if ($rps{$player}{item}[$typeid] =~ /^(\d+)(\D)$/) { $suffix=$2; $type=item_describe($typeid,"$1$2"); }
         $change = "${player}$change" .
             " $player\'s $type $change[$good] 10% of its effectiveness.";
         chanmsg_l($change);