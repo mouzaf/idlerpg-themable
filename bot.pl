6bdbfca7948e8ec8b86bcd9b153d3a999cba49a5
Phil Carmody <pc+dev@asdf.org>
2015-07-21 14:15:00 +0300

    bot.pl: fix mis-indexing of fragile items
    $_ is the number itself, which is the type id, not the index
    in the @fragileitems array where that number resides.
    
    Signed-off-by: Phil Carmody <pc+dev@asdf.org>

--- bot.pl
+++ bot.pl
@@ -2038,8 +2038,7 @@ sub modify_item($) {
     my @players = grep { $rps{$_}{online} } keys(%rps);
     return unless @players;
     my $player = $players[rand(@players)];
-    my @changeableitems = grep { $good ? $godsend[$fragileitems[$_]]
-				     : $calamity[$fragileitems[$_]]; } (@fragileitems);
+    my @changeableitems = grep { $good ? $godsend[$_] : $calamity[$_]; } (@fragileitems);
     if (@changeableitems and rand(10) < 1) {
 	my @change = ('loses', 'gains');
 	my $typeid = $changeableitems[rand(@changeableitems)];
